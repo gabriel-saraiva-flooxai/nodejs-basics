cc15e7353ef4c21f02c3403fb0da0074910c9136
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-03-27 14:29:45 -0300

    feat: add endpoint to fetch user by ID

--- routes/users.js
+++ routes/users.js
@@ -43,6 +43,21 @@ module.exports = (app) => {
         });
 
     });
-    
+
+    let routeID = app.route('/users/:id');
+
+    routeID.get((req, res) => {
+
+        db.findOne({_id:req.params.id}).exec((err, user) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(user);
+            }
+
+        });
+
+    });
 
 };
